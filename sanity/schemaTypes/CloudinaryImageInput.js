e845d5ded9fd469687b625b5db4d3cebe27fef59
kp1153 <kamta_tiwari@yahoo.com>
2025-11-29 10:37:29 +0530

    force deploy

--- sanity/schemaTypes/CloudinaryImageInput.js
+++ sanity/schemaTypes/CloudinaryImageInput.js
@@ -19,11 +19,11 @@ export default function CloudinaryImageInput(props) {
       try {
         const formData = new FormData();
         formData.append("file", file);
-        formData.append("upload_preset", "sanity");
+        formData.append("upload_preset", "kashmir");
         formData.append("folder", "sanity-images");
 
         const response = await fetch(
-          `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
+          `https://api.cloudinary.com/v1_1/diielqcd8/image/upload`,
           {
             method: "POST",
             body: formData,
